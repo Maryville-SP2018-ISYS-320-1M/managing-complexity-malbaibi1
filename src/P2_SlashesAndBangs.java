960c2a182adcd5900ff008b4f907eadfddfe94b0
Mansour Albaibi <mansouralbaibi@mansours-air.attlocal.net>
2018-03-24 18:47:09 -0500

    Revert "Updated"
    This reverts commit 45a2645d970ebc84d15f18a8129713e2036c4439.

--- src/P2_SlashesAndBangs.java
+++ src/P2_SlashesAndBangs.java
@@ -1,7 +1,7 @@
 /*
-   ISYS 320
-  	Name(Mansour Albaibi):
-  	Date: 3/24/2018  
+  	ISYS 320
+  	Name(s):
+  	Date: 
 */
 
 
@@ -11,23 +11,16 @@
  * how many of the character is printed on each line? Choose your headings based on how you can break down
  * the figure.
   
-Line Number  | Total Characters  | Back slashes on start | Exlamation marks  | Back slashes on end  | Relation  | 
-0                     22                 0                      22                    0                 back/start slashes = line number + line number
-1                     22                 2                      18                    2                 back/start slashes = line number + line number
-2                     22                 4                      14                    4                 back/start slashes = line number + line number
-3                     22                 6                      10                    6                 back/start slashes = line number + line number
-4                     22                 8                      6                     8                 back/start slashes = line number + line number
-5                     22                 10                     2                     10                back/start slashes = line number + line number
+Line Number		| HEADING		| HEADING		| HEADING		| HEADING		| HEADING		|	
+
+
+
+
 
 */
 
 /* 3. Using your analysis from above, in pseudocode describe the algorithm for how to draw the figure
  
- A.Make a method drawFigure.
- B.Create an outer loop to control number of lines.
- C.Create a loop inside outer loop to control Back slashes on start of each line.
- D.Create another loop inside outer loop to control Exlamation marks on each line.
- E.Create another loop inside outer loop to control Back slashes on end of each line.
  
  
  
@@ -36,29 +29,8 @@ Line Number  | Total Characters  | Back slashes on start | Exlamation marks  | B
 
 public class P2_SlashesAndBangs {
 
- public static void main(String[] args) {
-  drawFigure();
- }
- 
- 
- public static void drawFigure() {
-  int exl=22;
-  for( int i = 0; i <= 5; i++ ) {                       //loop to control number of lines
-    int mark=i+i;
-    for( int j = 1; j <= mark; j++ ) {                  //loop to control Back slashes on start of line
-    System.out.print( "/");
-    }
-    for( int k = 1; k <= exl; k++ ) {                   //loop to control Exlamation marks
-    System.out.print( "!");
-    }
-    for( int j = 1; j <= mark; j++ ) {                 //loop to control Back slashes on end of line
-    System.out.print( "/");
-    }
-    System.out.println();
-    exl=exl-4;
-    
-  }
-  
- }
+	public static void main(String[] args) {
+
+	}
 
 }
